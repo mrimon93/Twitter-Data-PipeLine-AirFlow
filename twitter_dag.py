1babd43050e931c3bb797aff9968ec218ad9e94e
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-01 23:44:49 +0100

    default_args

--- twitter_dag.py
+++ twitter_dag.py
@@ -3,4 +3,17 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
-from twitter_etl import run_twitter_etl
+from twitter_etl import run_twitter_etl #import entire function from the file
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+
